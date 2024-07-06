6f6a71504c477bbc352890691b44c6c09942530b
Vishnu K S <vishnu.ks@katzion.com>
2024-07-06 13:15:44 +0530

    vercel - analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Main from "./components/main/main";
 import Sidebar from "./components/sidebar/sidebar";
-
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
   return (
     <>
       <Sidebar />
       <Main />
+      <Analytics />
     </>
   );
 };
